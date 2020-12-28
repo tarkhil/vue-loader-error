20963dc6d400b93df8fd153ab9733a5a4f5b8663
Alex Povolotsky <tarkhil@corp.infotel.ru>
2020-12-28 15:56:52 +0300

    Vue цепляется

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+//import store from './store';
+//import CloseTariff from './components/CloseTariff.vue'
+
+var app = new Vue({
+//    store,
+    components: {
+//	CloseTariff
+    },
+    beforeCreate() {
+	console.log('before create');
+    },
+    mounted() {
+	console.log('mounted');
+    }
+}).$mount("#app");
